4e3d0148d9586d0a76eed8a0629a788d693c90a6
Daniel <38920805+VextoN@users.noreply.github.com>
2019-10-20 17:11:14 +0100

    test

--- MySphere.js
+++ MySphere.js
@@ -17,7 +17,7 @@ class MySphere extends CGFobject {
     this.texCoords = [];
 
     //goes around latitute of the sphere
-    for (let latNumber = -this.stacks; latNumber <= this.stacks; ++latNumber) {
+    for (let latNumber = 0; latNumber <= this.stacks; ++latNumber) {
       let theta = (latNumber * Math.PI/2) / this.stacks;
       let sinTheta = Math.sin(theta);
       let cosTheta = Math.cos(theta);
@@ -47,6 +47,37 @@ class MySphere extends CGFobject {
       }
     }
     
+    for (let latNumber = -this.stacks; latNumber <= 0; ++latNumber) {
+      let theta = (latNumber * Math.PI/2) / this.stacks;
+      let sinTheta = Math.sin(theta);
+      let cosTheta = Math.cos(theta);
+      //goes around longitude of the sphere
+      for (let longNumber = 0; longNumber <= this.slices; ++longNumber) {
+        let phi = longNumber * 2 * Math.PI / this.slices;
+        let sinPhi = Math.sin(phi);
+        let cosPhi = Math.cos(phi);
+        let x = cosTheta*cosPhi;
+        let y = cosTheta*sinPhi;
+        let z = sinTheta;
+        //vertices 
+        this.vertices.push(this.radius * x); //x = r * cos(theta) * cos(phi)
+        this.vertices.push(this.radius * y); //y = r * cos(theta) * sin(phi)
+        this.vertices.push(this.radius * z); //z = r * sin(theta)
+
+        //normals have the direction of the vertices coordinates
+        this.normals.push(x);
+        this.normals.push(y);
+        this.normals.push(z);
+        
+        //texCoords
+        let u = 1 - (longNumber / this.slices);
+        let v = 0.5 - (latNumber / this.stacks) / 2;
+        this.texCoords.push(u);
+        this.texCoords.push(v);
+      }
+    }
+    
+
     // Calculate sphere indices
     for (let latNumber = 0; latNumber <= this.stacks*2; ++latNumber) {
       for (let longNumber = 0; longNumber < this.slices; ++longNumber) {
