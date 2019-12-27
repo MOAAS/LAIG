2a278e64b8de86a6d4ff0eac64f8802f58016233
Daniel <38920805+VextoN@users.noreply.github.com>
2019-12-27 19:19:26 +0000

    cena

--- TP3/MyPlane.js
+++ TP3/MyPlane.js
@@ -4,16 +4,16 @@ class MyPlane extends CGFobject {
         this.nPartsU = nPartsU;
         this.nPartsV = nPartsV;
         this.controlPoints = [	// U = 0
+            [ // V = 0..1
+                [ 0.5, 0, -0.5, 1 ],
+                [ 0.5, 0, 0.5, 1 ]					 
+           ],
             [ // V = 0..1;
-                [-0.5, 0, 0.5, 1 ],
-                [-0.5, 0, -0.5, 1 ]
+                [-0.5, 0, -0.5, 1 ],
+                [-0.5, 0, 0.5, 1 ]
                
            ],
            // U = 1
-           [ // V = 0..1
-                [ 0.5, 0, 0.5, 1 ],						 
-                [ 0.5, 0, -0.5, 1 ]
-           ]
         ]
         this.degree1 = 1
         this.degree2 = 1
